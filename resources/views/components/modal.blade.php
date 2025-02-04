ddefdf9bdb0a839b142f879bc1aeca38d312b302
Vladimír Čamaj home <vladimir.c.amaj@lurity.com>
2025-02-05 00:04:32 +0100

    Modal dialog

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -1,14 +1,18 @@
-<!-- Modal -->
+<!-- This is universal modal component with optional $title and $content -->
+
 <div class="modal fade" id="{{$modal_id}}" tabindex="-1" aria-labelledby="{{$modal_id}}" aria-hidden="true">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
+
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">{{$title ?? 'Default title'}}</h5>
+                <h5 class="modal-title" id="exampleModalLabel">{{$title ?? 'No title'}}</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
+
             <div class="modal-body">
-                {{$content}}
+                {{$content ?? 'No content'}}
             </div>
+
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                 <button type="button" class="btn btn-primary">Save changes</button>
