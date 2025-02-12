f30d23c1c791218c219409de68ce2f25be14442b
Vladimír Čamaj home <vladimir.c.amaj@lurity.com>
2025-02-13 00:21:52 +0100

    alerts.blade.php update

--- resources/views/components/alerts.blade.php
+++ resources/views/components/alerts.blade.php
@@ -1,17 +1,21 @@
-<div class="position-absolute bottom-0 end-0 py-3 px-3 w-50">
+<div class="position-fixed bottom-0 end-0 px-3 w-50">
     @if (session('success'))
-        <div class="alert alert-success">
+        <div class="alert alert-success alert-dismissible fade show">
             {{ session('success') }}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
     @endif
     @if (session('error'))
-        <div class="alert alert-danger">
+        <div class="alert alert-danger alert-dismissible fade show">
             {{ session('error') }}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
     @endif
     @if (session('info'))
-        <div class="alert alert-info">
+        <div class="alert alert-info alert-dismissible fade show">
             {{ session('info') }}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
     @endif
 </div>
+
